addef3b506c64cfdea53f580cce44b3cfe2d2e4b
AddiS4 <addi.kumar@knoldus.in>
2019-07-11 13:44:04 +0530

    I added test

---
+++ build.sbt
@@ -0,0 +1,10 @@
+name := "test"
+
+organization := "xxx.xxxx.xxx"
+
+scalaVersion := "2.11.12"
+
+sbtVersion := "1.2.8"
+
+version := "1.0"
+
---
+++ src/main/scala/Hello.scala
@@ -0,0 +1,9 @@
+object Hello extends App {
+def Sum(a: Int): Unit= {
+var sum = 1 to 10
+for (i<-sum) {
+println(i)
+}
+println("Welcome to sbt")
+}
+}
